1d7a2f75a54ad4f4f318e5ab4a838fc75f108416
kathleen.e.burns@gmail.com <kathleen.e.burns@gmail.com>
2018-12-22 13:57:44 -0600

    updates to SQL

--- Katie working files/schema.sql
+++ Katie working files/schema.sql
@@ -1,4 +1,5 @@
 -- Drops the blogger if it exists currently --
-DROP DATABASE IF EXISTS blogger;
+DROP DATABASE IF EXISTS party;
 -- Creates the "blogger" database --
-CREATE DATABASE blogger;
+CREATE DATABASE party
+;
