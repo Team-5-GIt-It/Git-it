8ca2c15a53da71d7baab388ac192e8d59fd52a71
Abigail Jackson <41799687+abigailjxn@users.noreply.github.com>
2019-01-07 20:06:49 -0600

    added js from feature-host survey branch

---
+++ models/Event.js
@@ -0,0 +1,29 @@
+module.exports = function(sequelize, DataTypes) {
+  var Event = sequelize.define("Event", {
+    // Giving the Event model a name of type STRING
+    name: DataTypes.STRING,
+    event: DataTypes.STRING,
+    address: DataTypes.STRING,
+    city: DataTypes.STRING,
+    state: DataTypes.STRING,
+    zip: DataTypes.INTEGER,
+    date: DataTypes.DATEONLY,
+    timeStart: DataTypes.TIME,
+    timeEnd: DataTypes.TIME,
+    groupSize: DataTypes.STRING,
+    eventType: DataTypes.STRING,
+    attire: DataTypes.STRING,
+    description: DataTypes.TEXT,
+    eventCode: DataTypes.STRING
+  });
+
+  //   Event.associate = function(models) {
+  //     // Associating Author with Posts
+  //     // When an Author is deleted, also delete any associated Posts
+  //     Event.hasMany(models.Item, {
+  //       onDelete: "cascade"
+  //     });
+  //   };
+
+  return Event;
+};
